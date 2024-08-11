6df38e68c78cb491f90962031e0ff8a712bd29eb
adilya02 <54772375+adilya02@users.noreply.github.com>
2024-08-11 23:08:50 +0500

    Add files via upload
    Скрипт для вывода слова "crickets" каждый кадр

---
+++ crickets.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crickets : MonoBehaviour
+{
+    void Update()
+    {
+        // Выводим слово "crickets" в консоль
+        Debug.Log("crickets");
+    }
+}
